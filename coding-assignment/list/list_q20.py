6e543806183f6c7134f18f46464849a3bb253d57
Kratika Rathi <rathikratika26@gmail.com>
2024-11-28 11:31:31 +0530

    index

---
+++ coding-assignment/list/list_q20.py
@@ -0,0 +1,7 @@
+animals = ["cat", "dog", "rabbit", "bird"]
+length = len(animals)  # Finding the length of the list
+print(f"Length of the list: {length}")
+
+for index, animal in enumerate(animals):
+    print(f"{index}: {animal}")
+

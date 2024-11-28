cc987c1d260394be7330167f157b7bec9ad3931b
Kratika Rathi <rathikratika26@gmail.com>
2024-11-28 11:30:01 +0530

    iterate

---
+++ coding-assignment/list/list_q19.py
@@ -0,0 +1,9 @@
+colors = ["red", "green", "blue", "yellow", "purple"]
+
+# Slicing the list
+first_three_colors = colors[:3]
+print(first_three_colors)  # Output: ['red', 'green', 'blue']
+
+for color in colors:
+    print(color)
+

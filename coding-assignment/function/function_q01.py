d42d71eeebbea96875ade814d93e163a77481666
Kratika Rathi <rathikratika26@gmail.com>
2024-11-28 12:11:35 +0530

    function_q01.py

---
+++ coding-assignment/function/function_q01.py
@@ -0,0 +1,6 @@
+#1. Find the Maximum of Three Numbers
+#Write a function that takes three numbers and returns the maximum value.
+
+def max_of_three(a, b, c):
+    return max(a, b, c)
+print(max_of_three(2, 9, 5))function

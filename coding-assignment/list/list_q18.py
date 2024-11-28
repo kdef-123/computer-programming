3d39e3f493d437073430deb3ec5e5d6a263ac15c
Kratika Rathi <rathikratika26@gmail.com>
2024-11-28 11:29:14 +0530

    modify

---
+++ coding-assignment/list/list_q18.py
@@ -0,0 +1,5 @@
+numbers = [10, 20, 30, 40, 50]
+numbers.append(60)  # Adding 60
+numbers.remove(30)  # Removing 30
+numbers[0] = 5  # Changing the first element to 5
+print(numbers)  # Output: [5, 20, 40, 50, 60]
